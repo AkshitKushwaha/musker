38d73d603ce85dffaaaa7acf3161c95cdb1ee85d
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2024-01-07 13:49:17 +0530

    Added a login system

--- musker/views.py
+++ musker/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Meep
 from .forms import MeepForm
+from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
     if request.user.is_authenticated:
@@ -57,3 +59,27 @@ def profile(request, pk):
     else:
         messages.success(request, 'login to view this page')
         return redirect('home')
+    
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Logged in Successfully!')
+            return redirect('home')
+        else:
+            messages.success(request, 'Error logging in, please try again')
+            return redirect('login')
+
+
+    else:
+        return render(request, 'login.html', {})
+  
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Logged out')
+    return redirect('home')
