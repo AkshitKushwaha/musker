0d211584ac973ddc41e3d2eb13a49db7a0bdf2dc
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2024-01-11 22:00:48 +0530

    Added a registration page

--- musker/views.py
+++ musker/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Meep
-from .forms import MeepForm
+from .forms import MeepForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 def home(request):
@@ -83,3 +85,28 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'Logged out')
     return redirect('home')
+
+
+def register_user(request):
+
+    form = SignUpForm()
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #first_name = form.cleaned_data['first_name']
+            #last_name = form.cleaned_data['last_name']
+            #email = form.cleaned_data['email']
+
+            #Login User
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            messages.success(request, 'Registration Successful!')
+            return redirect('home')
+
+    return render(request, 'register.html', {'form': form})
