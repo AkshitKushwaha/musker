7969c926754101983a92f3a5db8210eb04823653
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2024-01-12 11:25:19 +0530

    Added an update profile page

--- musker/views.py
+++ musker/views.py
@@ -5,6 +5,7 @@ from .forms import MeepForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -110,3 +111,22 @@ def register_user(request):
             return redirect('home')
 
     return render(request, 'register.html', {'form': form})
+
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+
+        form = SignUpForm(request.POST or None, instance=current_user)
+        if form.is_valid():
+            form.save()
+            login(request, current_user)
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('home')
+
+
+        return render(request, 'update_user.html', {'form': form})
+    
+    else:
+        messages.success(request, 'login to view this page')
+        return redirect('home')
