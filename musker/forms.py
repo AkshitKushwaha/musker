6c4e4e0aee2016f106e2ab1b717a2594ec8799f2
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2024-01-26 20:17:37 +0530

    added profile images and update profile

--- musker/forms.py
+++ musker/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from .models import Meep
+from .models import Meep, Profile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+#Profile extras form
+class ProfilePicForm(forms.ModelForm):
+    profile_image = forms.ImageField(label="Profile Picture")
+    
+    class Meta:
+        model = Profile
+        fields = ('profile_image', )
 
 class MeepForm(forms.ModelForm):
     body = forms.CharField(required=True, widget = forms.widgets.Textarea(
--- musker/views.py
+++ musker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Meep
-from .forms import MeepForm, SignUpForm
+from .forms import MeepForm, SignUpForm, ProfilePicForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
@@ -117,15 +117,21 @@ def update_user(request):
     if request.user.is_authenticated:
         current_user = User.objects.get(id=request.user.id)
 
-        form = SignUpForm(request.POST or None, instance=current_user)
-        if form.is_valid():
-            form.save()
+        profile_user = Profile.objects.get(user__id=request.user.id)
+
+        user_form = SignUpForm(request.POST or None, request.FILES or None,  instance=current_user)
+
+        profile_form = ProfilePicForm(request.POST or None, request.FILES or None, instance=profile_user)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            
             login(request, current_user)
             messages.success(request, 'Your profile has been updated!')
             return redirect('home')
 
 
-        return render(request, 'update_user.html', {'form': form})
+        return render(request, 'update_user.html', {'user_form': user_form, 'profile_form': profile_form})
     
     else:
         messages.success(request, 'login to view this page')
